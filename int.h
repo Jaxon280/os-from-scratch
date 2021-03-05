701c8408f6c263bb4f082114827f6dfa1cf8ec91
飯田夏生 <keresu0720@icloud.com>
2021-03-05 19:55:39 +0900

    add int.h

---
+++ int.h
@@ -0,0 +1,6 @@
+typedef unsigned int uint;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long uint64;
--- start.c
+++ start.c
@@ -1,4 +1,4 @@
-#include "stdint.h"
+#include "int.h"
 
 #define MPP_MMASK 0b11 << 11
 #define MIE_MASK (1 << 3) | (1 << 7)
@@ -7,42 +7,42 @@ __attribute__((aligned(16))) char _stack[4096]; // aligned here because stack po
 
 void main();
 
-static inline void set_mstatus(uint64_t *x)
+static inline void set_mstatus(uint64 *x)
 {
     *x |= MIE_MASK;
     *x &= ~MPP_MMASK;
     *x |= (0b1 << 11); // set MPP to use Supervisor mode.
 }
 
-static inline void w_mstatus(uint64_t x)
+static inline void w_mstatus(uint64 x)
 {
     asm volatile("csrw mstatus, %0"
                  :
                  : "r"(x));
 }
 
-static inline void w_mepc(uint64_t x)
+static inline void w_mepc(uint64 x)
 {
     asm volatile("csrw mepc, %0"
                  :
                  : "r"(x)); // set address retured from mret
 }
 
-static inline void w_satp(uint64_t x)
+static inline void w_satp(uint64 x)
 {
     asm volatile("csrw satp, %0"
                  :
                  : "r"(x)); // clear satp(pgdir address register) here.
 }
 
-static inline void w_medeleg(uint64_t x)
+static inline void w_medeleg(uint64 x)
 {
     asm volatile("csrw medeleg, %0"
                  :
                  : "r"(x)); // delegation to supervisor mode.
 }
 
-static inline void w_mideleg(uint64_t x)
+static inline void w_mideleg(uint64 x)
 {
     asm volatile("csrw mideleg, %0"
                  :
@@ -51,12 +51,12 @@ static inline void w_mideleg(uint64_t x)
 
 void start()
 {
-    uint64_t mstat = 0;
+    uint64 mstat = 0;
     set_mstatus(&mstat);
 
     w_mstatus(mstat);
 
-    w_mepc((uint64_t)main);
+    w_mepc((uint64)main);
 
     w_satp(0);
 
